40cea13738513ed26752dd3efd0503381f355522
chentianwei <ctw.3@163.com>
2017-11-23 10:41:18 +0800

    add 3 colomn and validates

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ApplicationRecord
-  validates :title, :description, presence:true
+  validates :title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,presence:true
+  validates :wage_lower_bound,:wage_upper_bound, numericality: {greater_than: 0}
 end

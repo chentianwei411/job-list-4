09110dd5b13258d2b476d8e48af72c0eb1b6540f
chentianwei <ctw.3@163.com>
2017-11-26 21:21:43 +0800

    order by wage

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "建立默认种子用户"
-  User.create(email:"1@1.com",password:"123456",password_confirmation:"123456",is_admin:true)
+# puts "建立默认种子用户"
+  # User.create(email:"1@1.com",password:"123456",password_confirmation:"123456",is_admin:true)
 puts "建立默认jobs"
 for i in 1..10 do
-  Job.create(title:"No#{i}",description:"这是关于No#{i}的介绍")
+  Job.create(title:"No#{i}",description:"这是关于No#{i}的介绍",wage_upper_bound: rand(50..99)*100,
+              wage_lower_bound:rand(10..50)*50,is_hidden:"false",contact_email:"11e")
 end
+puts "？"
